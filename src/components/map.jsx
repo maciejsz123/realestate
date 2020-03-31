1b42e70c70568a6f00fa66e8a1a48c6345ab86f0
maciej <maciejsz0@o2.pl>
2020-03-31 18:54:17 +0200

    map marker on hover

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -1,9 +1,28 @@
 import React, {Component} from 'react';
 import { Map, Marker, Popup, TileLayer, Tooltip } from 'react-leaflet';
-import { Icon } from 'leaflet';
+import L, { Icon } from 'leaflet';
 import { connect } from 'react-redux';
 
 class HousesMap extends Component {
+  constructor() {
+    super();
+    this.state = {
+      hoverMarker: []
+    }
+  }
+
+  hoverItem() {
+    this.setState( {
+      hoverMarker: [[40,40]]
+    });
+  }
+
+  hoverOutItem() {
+    this.setState( {
+      hoverMarker: []
+    })
+  }
+
   render() {
     const position = [51.1078852, 17.0385376]
     const icon = new Icon({
@@ -17,15 +36,25 @@ class HousesMap extends Component {
       </Marker>
       )
     );
+    const markers2 = this.state.hoverMarker.map( (item, i) => (
+      <Marker icon={icon} key={i} position={item}>
+        <Popup onOpen={() => console.log('dd')}>sasdf</Popup>
+      </Marker>
+      )
+    );
 
     return(
+      <div>
+        <div onMouseOver={this.hoverItem.bind(this)} onMouseOut={this.hoverOutItem.bind(this)}>wHAT</div>
         <Map center={position} zoom={6} style={{width: '1000px', height: '1000px'}}>
           <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a>"
           />
           {markers}
+          {markers2}
         </Map>
+        </div>
     );
   }
 }
