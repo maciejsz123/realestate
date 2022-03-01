ce5d063947c6713d994b1d8579dc46644579da18
maciej <maciejsz0@o2.pl>
2022-03-01 22:41:26 +0100

    Updates

---
+++ precache-manifest.a196eccb5c7776326a7300c418c8d809.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35725aa9cf6f048d36c9063f463931bc",
+    "url": "/realestate/index.html"
+  },
+  {
+    "revision": "c64e5cd46c90f3c36014",
+    "url": "/realestate/static/css/main.5b765125.chunk.css"
+  },
+  {
+    "revision": "943ce1234c486224c7fa",
+    "url": "/realestate/static/js/2.4d8ea464.chunk.js"
+  },
+  {
+    "revision": "c64e5cd46c90f3c36014",
+    "url": "/realestate/static/js/main.29c443e2.chunk.js"
+  },
+  {
+    "revision": "76d97180819cdd75db1a",
+    "url": "/realestate/static/js/runtime-main.2a84039a.js"
+  },
+  {
+    "revision": "febef9c2b0046b70f0e932ec5503a57c",
+    "url": "/realestate/static/media/bluedot.febef9c2.png"
+  },
+  {
+    "revision": "e01eba94ceb9fa6ca7e7df4e3029a992",
+    "url": "/realestate/static/media/face1.e01eba94.jpg"
+  },
+  {
+    "revision": "683a26c9b9a555f1a0c678400387c9ab",
+    "url": "/realestate/static/media/face2.683a26c9.jpg"
+  },
+  {
+    "revision": "c51f128d11f518fb6a56df4a5ac8f0c6",
+    "url": "/realestate/static/media/face3.c51f128d.jpg"
+  },
+  {
+    "revision": "ef76fc3f1269fd2c73151933995746c6",
+    "url": "/realestate/static/media/face4.ef76fc3f.jpg"
+  },
+  {
+    "revision": "2ae96f631f56b0e0e181e85a46ddbd2a",
+    "url": "/realestate/static/media/greendot.2ae96f63.png"
+  },
+  {
+    "revision": "984050b4dfc595a428d338f4e87fc2a3",
+    "url": "/realestate/static/media/room1.984050b4.jpg"
+  },
+  {
+    "revision": "81e667d3837efebcfe8a01296ae241a4",
+    "url": "/realestate/static/media/room2.81e667d3.jpg"
+  },
+  {
+    "revision": "ac2500e8560b07820bea9deab51408a5",
+    "url": "/realestate/static/media/room3.ac2500e8.jpg"
+  },
+  {
+    "revision": "b9362397a7a31c90f480c3157f378216",
+    "url": "/realestate/static/media/room4.b9362397.jpg"
+  },
+  {
+    "revision": "8ec28c1c8737d4add08f20072bc984dc",
+    "url": "/realestate/static/media/room5.8ec28c1c.jpg"
+  }
+]);
