9bebe3f12676f2a1bf93d3bbc0fd56ef2f486c41
maciej <maciejsz0@o2.pl>
2022-03-01 21:58:33 +0100

    Updates

---
+++ precache-manifest.92f1281b39df337a996ad975a4cf6742.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8dd5449a87f5385302147e0df82cd38",
+    "url": "/restaurant/index.html"
+  },
+  {
+    "revision": "772cb3131810b14d8b08",
+    "url": "/restaurant/static/css/main.66a95215.chunk.css"
+  },
+  {
+    "revision": "9adc0e6c5af4c681f0d9",
+    "url": "/restaurant/static/js/2.0e34a3cd.chunk.js"
+  },
+  {
+    "revision": "772cb3131810b14d8b08",
+    "url": "/restaurant/static/js/main.2fde8abd.chunk.js"
+  },
+  {
+    "revision": "3c1456dc91f12454c1fa",
+    "url": "/restaurant/static/js/runtime-main.9e15065d.js"
+  },
+  {
+    "revision": "febef9c2b0046b70f0e932ec5503a57c",
+    "url": "/restaurant/static/media/bluedot.febef9c2.png"
+  },
+  {
+    "revision": "e01eba94ceb9fa6ca7e7df4e3029a992",
+    "url": "/restaurant/static/media/face1.e01eba94.jpg"
+  },
+  {
+    "revision": "683a26c9b9a555f1a0c678400387c9ab",
+    "url": "/restaurant/static/media/face2.683a26c9.jpg"
+  },
+  {
+    "revision": "c51f128d11f518fb6a56df4a5ac8f0c6",
+    "url": "/restaurant/static/media/face3.c51f128d.jpg"
+  },
+  {
+    "revision": "ef76fc3f1269fd2c73151933995746c6",
+    "url": "/restaurant/static/media/face4.ef76fc3f.jpg"
+  },
+  {
+    "revision": "2ae96f631f56b0e0e181e85a46ddbd2a",
+    "url": "/restaurant/static/media/greendot.2ae96f63.png"
+  },
+  {
+    "revision": "984050b4dfc595a428d338f4e87fc2a3",
+    "url": "/restaurant/static/media/room1.984050b4.jpg"
+  },
+  {
+    "revision": "81e667d3837efebcfe8a01296ae241a4",
+    "url": "/restaurant/static/media/room2.81e667d3.jpg"
+  },
+  {
+    "revision": "ac2500e8560b07820bea9deab51408a5",
+    "url": "/restaurant/static/media/room3.ac2500e8.jpg"
+  },
+  {
+    "revision": "b9362397a7a31c90f480c3157f378216",
+    "url": "/restaurant/static/media/room4.b9362397.jpg"
+  },
+  {
+    "revision": "8ec28c1c8737d4add08f20072bc984dc",
+    "url": "/restaurant/static/media/room5.8ec28c1c.jpg"
+  }
+]);
